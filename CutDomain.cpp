9e37b1581c709fcef8ecbbf32f57daa5abaf9a82
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2023-07-27 22:16:08 +0900

    Updated for verbose

--- CutDomain.cpp
+++ CutDomain.cpp
@@ -27,24 +27,16 @@ void CutDomain::cutDomain(Domain& dom, CutSites& cut_sites, PDPDistanceMatrix& p
     site = cut.cut(ca, dom, val, dist, pdpMatrix);
     //printf("site %i \n",site)   ;
     if (site < 0) {
-        //printf("site<0\n");
-        domains.push_back(dom);
-        dom.setScore(val.s_min);
-        ndom++;
-        return;
+      //printf("site<0\n");
+      dom.setScore(val.s_min);
+      domains.push_back(dom);      
+      ndom++;
+      return;
     }
     
-    //printf("Ndomain=%i\n",domains.size());
-    //printf("Ndomain=%i\n",(int)domains.size());
-//cut_sites.cut_sites[cut_sites.ncuts++] = site;
-//cut_sites.cut_sites[cut_sites.ncuts++] = site;
     cut_sites.addNcuts(1);
-    //cut_sites.setNcuts(cut_sites.getNcuts(),site);
-    //printf("Line0001\n");
-    //printf("getNcuts%i\n",cut_sites.getNcuts());
-    //printf("sizesize=%i",cut_sites.cut_sites.size());
     cut_sites.cut_sites[cut_sites.getNcuts()]=site;
-    //printf("Line0002\n");
+
     dom1.setSize(0);
     dom1.setNseg(0);
     dom2.setSize(0);
@@ -61,33 +53,15 @@ void CutDomain::cutDomain(Domain& dom, CutSites& cut_sites, PDPDistanceMatrix& p
                 dom2.getSegmentAtPos(dom2.getNseg()).setFrom(dom.getSegmentAtPos(i).getFrom());
                 dom2.addNseg(1);
                 dom2.addSize(dom.getSegmentAtPos(i).getTo() - dom.getSegmentAtPos(i).getFrom() + 1);
-            } else if (site == dom.getSegmentAtPos(i).getFrom()) {
-                dom2.getSegmentAtPos(dom2.getNseg()).setTo(site-1);
-                dom2.getSegmentAtPos(dom2.getNseg()).setFrom(site-1);
-                dom2.addNseg(1);
-                dom1.getSegmentAtPos(dom1.getNseg()).setTo(site);
-                dom1.getSegmentAtPos(dom1.getNseg()).setFrom(site);
-                dom1.addNseg(1);
-                dom1.addSize(1);
-                dom2.addSize(1);
-            } else if (site == dom.getSegmentAtPos(i).getTo()) {
-                dom1.getSegmentAtPos(dom1.getNseg()).setTo(site);
-                dom1.getSegmentAtPos(dom1.getNseg()).setFrom(site);
-                dom1.addNseg(1);
-                dom2.getSegmentAtPos(dom2.getNseg()).setTo(site+1);
-                dom2.getSegmentAtPos(dom2.getNseg()).setFrom(site+1);
-                dom2.addNseg(1);
-                dom1.addSize(1);
-                dom2.addSize(1);
             } else if (site > dom.getSegmentAtPos(i).getFrom() && site < dom.getSegmentAtPos(i).getTo()) {
-                dom1.getSegmentAtPos(dom1.getNseg()).setTo(site);
-                dom1.getSegmentAtPos(dom1.getNseg()).setFrom(dom.getSegmentAtPos(i).getFrom());
-                dom1.addNseg(1);
-                dom2.getSegmentAtPos(dom2.getNseg()).setTo(dom.getSegmentAtPos(i).getTo());
-                dom2.getSegmentAtPos(dom2.getNseg()).setFrom(site+1);
-                dom2.addNseg(1);
-                dom1.addSize(site - dom.getSegmentAtPos(i).getFrom() + 1);
-                dom2.addSize(dom.getSegmentAtPos(i).getTo() - site + 1);
+	      dom1.getSegmentAtPos(dom1.getNseg()).setFrom(dom.getSegmentAtPos(i).getFrom());
+	      dom1.getSegmentAtPos(dom1.getNseg()).setTo(site-1);
+	      dom1.addNseg(1);
+	      dom1.addSize(site - dom.getSegmentAtPos(i).getFrom());
+	      dom2.getSegmentAtPos(dom2.getNseg()).setTo(dom.getSegmentAtPos(i).getTo());
+	      dom2.getSegmentAtPos(dom2.getNseg()).setFrom(site);
+	      dom2.addNseg(1);
+	      dom2.addSize(dom.getSegmentAtPos(i).getTo() - site + 1);
             }
 
         }
